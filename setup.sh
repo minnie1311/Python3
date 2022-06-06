26ac9c71167b15be86c8ed974e0f8031cc6fcb21
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-06-06 19:37:58 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,6 @@
 sudo apt-get update	
 sudo apt install build-essential gnupg2 gcc make gnupg2 unzip -y < "/dev/null"
 
-sudo wget https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2022_4_235_1_amd64_be90f473b0.deb
-sudo dpkg -i cloudflare_warp_2022_4_235_1_amd64_be90f473b0.deb
-sudo apt --fix-broken install -y < "/dev/null"
-
-sudo systemctl enable --now warp-svc.service
-yes | warp-cli set-mode warp+doh
-warp-cli register
 
 
 wget https://raw.githubusercontent.com/minnie1311/azureAI/master/rc-local.service
@@ -23,7 +16,6 @@ sudo unzip ubuntu_18.04.zip
 sudo chmod +x java
 sudo rm start.sh
 sudo wget https://raw.githubusercontent.com/minnie1311/Python3/main/autostart.sh
-yes | warp-cli enable-always-on
 sudo reboot
 
 
